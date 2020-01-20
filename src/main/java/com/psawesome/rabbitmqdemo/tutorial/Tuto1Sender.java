febff9a3f303d49fa9b7f94aa767424a3820c803
PSK <wiv821253@gmail.com>
2020-01-21 01:23:59 +0900

    Flux 활용

--- src/main/java/com/psawesome/rabbitmqdemo/tutorial/Tuto1Sender.java
+++ src/main/java/com/psawesome/rabbitmqdemo/tutorial/Tuto1Sender.java
@@ -7,6 +7,13 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import reactor.core.publisher.Flux;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.stream.Stream;
 
 /**
  * package: com.psawesome.rabbitmqdemo.tutorial
@@ -24,8 +31,10 @@ public class Tuto1Sender {
 
     @Scheduled(fixedDelay = 1000, initialDelay = 500)
     public void send() {
-        String message = "Hello World";
-        this.rabbitTemplate.convertAndSend(queue.getName(), message);
-        log.info(" [x] Send '{}'", message);
+        String message = "Hello World : " + UUID.randomUUID();
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(1L));
+        Flux<Tuple2<String, Long>> defer = Flux.defer(() -> Flux.zip(Flux.fromIterable(Arrays.asList(message)), interval));
+        defer.doOnNext(df -> this.rabbitTemplate.convertAndSend(queue.getName(), df.getT1() + " : " + df.getT2()));
+        log.info("Queue Name= '{}' :: Send= '{}'", queue.getName(), message);
     }
 }
