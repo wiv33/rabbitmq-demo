1996f02df8ee46762690ab3fb825fd32dbce5b45
PSK <wiv821253@gmail.com>
2020-01-25 02:02:02 +0900

    주석 정리, 불필요한 파일 삭제

--- src/main/java/com/psawesome/rabbitmqdemo/RabbitmqDemoApplication.java
+++ src/main/java/com/psawesome/rabbitmqdemo/RabbitmqDemoApplication.java
@@ -11,20 +11,29 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class RabbitmqDemoApplication {
 
     public static void main(String[] args) {
+//        tut1
+//        System.setProperty("spring.profiles.active", "tut1,hello-world,receiver");
+//        System.setProperty("spring.profiles.active", "tut1,hello-world,sender");
 
 //        tut2
-//        System.setProperty("spring.profiles.active", "tut2,work-queues,receiver,sender");
+//        System.setProperty("spring.profiles.active", "tut2,work-queues,sender");
+//        System.setProperty("spring.profiles.active", "tut2,work-queues,receiver");
 
 //        tut3
-//        System.setProperty("spring.profiles.active", "tut3,pub-sub,publish-subscribe,receiver,sender");
+//        System.setProperty("spring.profiles.active", "tut3,pub-sub,publish-subscribe,sender");
+//        System.setProperty("spring.profiles.active", "tut3,pub-sub,publish-subscribe,receiver");
 
 //        tut4
+//        System.setProperty("spring.profiles.active", "tut4,routing,receiver");
+//        System.setProperty("spring.profiles.active", "tut4,routing,sender");
 
 //        tut5
-//        System.setProperty("spring.profiles.active", "tut5,sender,receiver,topics");
+//        System.setProperty("spring.profiles.active", "tut5,sender,topics");
+//        System.setProperty("spring.profiles.active", "tut5,receiver,topics");
 
 //        tut6
-        System.setProperty("spring.profiles.active", "tut6,client,server,rpc");
+//        System.setProperty("spring.profiles.active", "tut6,server,rpc");
+//        System.setProperty("spring.profiles.active", "tut6,client,rpc");
 
         SpringApplication.run(RabbitmqDemoApplication.class, args);
 
