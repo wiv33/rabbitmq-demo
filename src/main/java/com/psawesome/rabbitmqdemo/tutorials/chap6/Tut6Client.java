d5690e9c1a274123b38d6647b78abfb1f99e865e
PSK <wiv821253@gmail.com>
2020-01-25 00:01:54 +0900

    create Tut6 Client

---
+++ src/main/java/com/psawesome/rabbitmqdemo/tutorials/chap6/Tut6Client.java
@@ -0,0 +1,32 @@
+package com.psawesome.rabbitmqdemo.tutorials.chap6;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * package: com.psawesome.rabbitmqdemo.tutorials.chap6
+ * author: PS
+ * DATE: 2020-01-24 금요일 23:57
+ */
+@Slf4j
+public class Tut6Client {
+
+    @Autowired
+    private RabbitTemplate template;
+
+    @Autowired
+    private DirectExchange directExchange;
+
+    int start = 0;
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 300)
+    public void send() {
+        log.info(" [x] Requesting fib({})", start);
+        Integer response = (Integer) template.convertSendAndReceive(directExchange.getName(), "rpc", start++);
+        log.info(" [.] Got '{}'", response);
+
+    }
+}
